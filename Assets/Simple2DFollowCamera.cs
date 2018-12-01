f80d13dadf1f161d9e3705d8c250393f0f0057f1
Jesse Fish <jesfish@gmail.com>
2018-11-30 20:58:04 -0800

    Added background moon

---
+++ Assets/Simple2DFollowCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Simple2DFollowCamera : MonoBehaviour {
+
+    public float speed = 15f;
+    public float minDistanceX;
+    public float minDistanceY;
+
+    public GameObject target;
+    public Vector3 offset;
+
+    private Vector3 targetPos;
+
+    // Use this for initialization
+    void Start() {
+        targetPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (target) {
+            Vector3 posNoZ = transform.position + offset;
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+            bool noXVal = Mathf.Abs(targetDirection.x) < minDistanceX;
+            bool noYVal = Mathf.Abs( targetDirection.y) < minDistanceY;
+            float interpVelocity = targetDirection.magnitude * speed;
+            Vector3 applyMask = new Vector3(noXVal ? 0 : 1, noYVal ? 0 : 1, 1);
+
+            applyMask.Scale(targetDirection.normalized * interpVelocity * Time.deltaTime);
+            targetPos = (transform.position) + applyMask;
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.25f);
+
+        }
+    }
+}
