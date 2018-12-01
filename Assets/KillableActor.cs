668fab20743ce5ebe96145395e557ad913dbf5ba
Jesse Fish <jesfish@gmail.com>
2018-12-01 02:48:25 -0800

    Added monster and cinemachine plugin

---
+++ Assets/KillableActor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillableActor : MonoBehaviour {
+    
+    protected Animator animator;
+
+    // Use this for initialization
+    protected void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Death() {
+        animator.SetTrigger("Death");
+    }
+    
+    public void BloodPickedUp() {
+        animator.SetTrigger("BloodPickedUp");
+    }
+}
--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -2,21 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Innocent : KillableActor {
-
-    [SerializeField] GameObject sprite;
+public class Monster : KillableActor {
 
 	// Use this for initialization
 	new void Start () {
         base.Start();
-    }
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    
-    public void ChangeSpriteLocation() {
-        sprite.transform.localPosition = new Vector3(0.452f, 0);
-    }
 }
