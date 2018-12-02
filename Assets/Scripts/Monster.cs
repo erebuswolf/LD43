12abafc5d653d99dcbee915b0b472b3d62a24b52
Jesse Fish <jesfish@gmail.com>
2018-12-01 22:51:27 -0800

    Monster push away working

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -11,5 +11,20 @@ public class Monster : KillableActor {
 	
 	// Update is called once per frame
 	void Update () {
+
+    }
+
+    private void FixedUpdate() {
+        PushAway();
+    }
+
+    void PushAway () {
+        var monsters = Physics2D.OverlapCircleAll(this.transform.position, .3f, LayerMask.GetMask("Monster"));
+        foreach (var m in monsters) {
+            if (m.gameObject != this.gameObject) {
+                Vector2 diff = (m.gameObject.transform.position - this.transform.position );
+                m.GetComponent<Rigidbody2D>().AddForce(diff.normalized * 5);
+            }
+        }
     }
 }
